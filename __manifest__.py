7f4f36d7fc5219007dcf2bf8d9ea9d3177360e0d
Sathir M. Mansoor <sathir@codeso.lk>
2021-02-07 22:51:57 +0530

    -> Template for development initialized

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Dinuth Cey Foods",
+    'version': '1.0.0',
+    'sequence': 1,
+    'depends': ['base','hr'],
+    'author': "Sathir M. Mansoor, ©2020. Fortrax Solutions All Rights Reserved.",
+    'license':"OPL-1",
+    'category': 'Specific Industry Applications',
+    'description': "Dinuth Cey Foods wants to achieve with the ERP product and has been prepared based on the client specifications and after a thorough analysis of the client company.",
+
+    'images' : [],
+    'qweb': [],
+    
+    # data files always loaded at installation
+    'data': [ ],
+
+    # website
+    'website': 'https://codeso.lk/',
+    
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
